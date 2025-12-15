e92180760b80b7e83ed847fda09244a4754df251
Jorge Chavez <jorgechavzns@gmail.com>
2025-12-15 02:04:29 -0800

    Add heatshrink dependency and update main function to extract frames from video

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ mod utils;
 
 use std::path::Path;
 use utils::frame_writer::process_and_write_frames;
+use utils::ffmpeg::extract_frames;
 
 
 fn main() {
     let frames_dir = Path::new("frames");
+    extract_frames("assets/video.mp4", "frames");
     process_and_write_frames(frames_dir);
+
+
 }
